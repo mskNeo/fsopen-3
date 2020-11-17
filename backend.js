2e70b2ab92f4a1ad8152e5e539c7eba38ebc0b7f
Mahdeen (Neo) Khan <mahdeen.s.khan@yale.edu>
2020-11-16 23:19:20 -0500

    build frontend

--- backend.js
+++ backend.js
@@ -28,6 +28,7 @@ let persons = [
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static('build'));
 
 morgan.token('body', (req) => {
   if (req.method === 'POST') return JSON.stringify(req.body);
---
+++ build/precache-manifest.7ceee1b5b6f3337440af26cee0944021.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dae5a81e03228f15d8261ed2c37a17ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8bf862dd9f06b675f4db",
+    "url": "/static/css/main.e7a6f5e1.chunk.css"
+  },
+  {
+    "revision": "6fa96f4e77e54634d75a",
+    "url": "/static/js/2.89f8abb7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.89f8abb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bf862dd9f06b675f4db",
+    "url": "/static/js/main.65170227.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
