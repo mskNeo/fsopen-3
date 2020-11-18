d67abb9ed6776596669e9e7a64dcefe8f342e753
Mahdeen (Neo) Khan <mahdeen.s.khan@yale.edu>
2020-11-18 16:12:40 -0500

    added mongoDB Atlas connection to backend

--- backend.js
+++ backend.js
@@ -1,31 +1,10 @@
+require('dotenv').config()
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const Person = require('./models/person');
+
 const app = express();
-
-let persons = [
-  {
-    "name": "Arto Hellas",
-    "number": "040-123456",
-    "id": 1
-  },
-  {
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523",
-    "id": 2
-  },
-  {
-    "name": "Dan Abramov",
-    "number": "12-43-234345",
-    "id": 3
-  },
-  {
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122",
-    "id": 4
-  }
-];
-
 app.use(express.static('build'));
 app.use(express.json());
 app.use(cors());
@@ -37,12 +16,12 @@ morgan.token('body', (req) => {
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
-app.get('/info', (request, response) => {
-  response.send(`<p>Phonebook has info for ${persons.length} people</p> <p>${new Date()}</p>`)
-})
-
 app.get('/api/persons', (request, response) => {
-  response.json(persons);
+  Person
+    .find({})
+    .then(people => {
+      response.json(people);
+    });
 })
 
 app.get('/api/persons/:id', (request, response) => {
@@ -104,7 +83,7 @@ app.post('/api/persons', (request, response) => {
   response.json(persons);
 })
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
 });
